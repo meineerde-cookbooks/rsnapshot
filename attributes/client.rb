c51f4eca0dddb1e24230898a018ae25e5f2bb1d4
Holger Just <hjust@meine-er.de>
2015-02-11 18:43:11 +0100

    Allow to set the nice and ionice values of the rsync sender process

--- attributes/client.rb
+++ attributes/client.rb
@@ -3,5 +3,7 @@ default['rsnapshot']['client']['create_user'] = true
 default['rsnapshot']['client']['manage_authorized_keys'] = true
 
 default['rsnapshot']['client']['rsync_path'] = '/usr/bin/rsync'
+default['rsnapshot']['client']['rsync_nice'] = nil
+default['rsnapshot']['client']['rsync_ionice'] = nil
 
 default['rsnapshot']['client']['paths'] = []
